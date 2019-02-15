37c29c59165769a4c693bbb59303d833964447fe
Mark McKelvy <mmckelvy@gmail.com>
2019-02-15 12:10:38 -0800

    create work order from the API

--- api/create-work-order.js
+++ api/create-work-order.js
@@ -1,3 +1,32 @@
+const Boom = require('boom')
+const { requestAsync: request } = require('./utils')
+
 module.exports = async function createWorkOrder(req, res, next) {
-  res.send('Success!')
+  try {
+    const [ scheme, token ] = req.header('authorization').split(' ')
+
+    /*
+    Potentially do some basic validation
+    */
+
+    const { response, body } = await request({
+      method: 'POST',
+      url: 'https://api.onupkeep.com/api/v2/work-orders',
+      json: req.body,
+      headers: {
+        'Session-Token': token,
+      }
+    })
+
+    // Assume any non 200 response is this server's fault.
+    if (response.statusCode !== 200) {
+      throw Boom.internal()
+    }
+
+    res.status(200).json(body)
+
+  } catch (err) {
+    // Pass the error to the main error handler
+    next(err)
+  }
 }
